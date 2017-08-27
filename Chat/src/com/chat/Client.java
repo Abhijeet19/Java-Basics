5abfd0b09e6413236c8d49b7c5d25ae63e8babe0
Abhijeet19 <abhijeet.patil19@gmail.com>
2017-08-27 15:54:37 +0530

    Add files via upload

---
+++ Chat/src/com/chat/Client.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.chat;
+
+/**
+ * @author Abhijeet
+ *
+ */
+import java.io.*;
+import java.net.*;
+
+public class Client {
+  public static void main(String[] args) throws Exception {
+	  
+     Socket sock = new Socket("127.0.0.1", 3000);                    
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));      
+     OutputStream ostream = sock.getOutputStream();
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+     System.out.println("To start the chat, type and press Enter key");
+ 
+     String receiveMessage, sendMessage;              
+     while(true) {
+        sendMessage = keyRead.readLine();  
+        pwrite.println(sendMessage);       
+        pwrite.flush();                    
+        if((receiveMessage = receiveRead.readLine()) != null) {
+            System.out.println(receiveMessage); 
+        }        
+      }              
+    }                    
+}                        
